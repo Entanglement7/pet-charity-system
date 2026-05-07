d9790f8ae71a27dec2ba8ca2b85badc7e54054a9
董轩哲 <3313758055@qq.com>
2026-05-07 10:34:10 +0800

    update

--- init.sql
+++ init.sql
@@ -268,3 +268,18 @@ INSERT IGNORE INTO audit_logs (auditor_id, ref_type, ref_id, action, reason) VAL
 (1, 'donation', 1, 'approve', '物品符合捐赠标准'),
 (1, 'donation', 2, 'approve', '物品符合捐赠标准'),
 (1, 'donation', 3, 'approve', '物品符合捐赠标准');
+
+ALTER TABLE organizations MODIFY COLUMN license_url TEXT;
+
+ALTER TABLE applications CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE audit_logs CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE donations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE exchange_items CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE exchange_records CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE items CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE logistics CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE organization_addresses CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE organizations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE points CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE points_records CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
